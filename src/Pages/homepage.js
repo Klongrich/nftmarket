782da5dc42072cb7b444b5de26c055b22179e8ba
klongrich <longrichk@gmail.com>
2021-08-30 15:52:45 -0400

    adding search by wallet page

--- src/Pages/homepage.js
+++ src/Pages/homepage.js
@@ -7,22 +7,39 @@ const Background = styled.div`
     padding-top: 30px;
     padding-left: 15px;
 
-    height: 1200px;
+    height: 100%;
 `
 
-const SearchAddress = styled.input`
+const ImageBox = styled.div`
+    height: 300px;
     width: 300px;
-    height: 22px;
-    border-radius: 3px;
+
+    padding: 20px;
+    
+    margin-left: 15px;
+    margin-right: 15px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+
+    border: 1px solid black;
+    display: inline-block;
 `
 
-const SearchButton = styled.button`
-
-`
+export const Filler = [
+    {
+      id: 0,
+      data_image_url: "null",
+    },
+    {
+      id: 1,
+      data_image_url: "null",
+    }
+]
 
 export default function Homepage() {
 
     const [SearchAddress, setSearchAddress] = useState('');
+    const [userNFTs, setUserNFTs] = useState(Filler);
 
     function getCurrentAssests() {
         fetch(
@@ -38,6 +55,10 @@ export default function Homepage() {
             .then((response) => response.json())
             .then((data) => {
                 console.log(data);
+                setUserNFTs(data.assets);
+
+                console.log(data.assets[0])
+                console.log(data.assets[0].image_url)
             })
             .catch((err) => {
                 console.log(err)
@@ -65,12 +86,25 @@ export default function Homepage() {
 
             <br />
 
-            <p> Search for NFT Collection</p>
+            <p> Search for NFT Collection By Wallet</p>
             <input onChange={e => setSearchAddress(e.target.value)} />
-            <SearchButton onClick={() => getCurrentAssests()}> Search </SearchButton>
+            <button onClick={() => getCurrentAssests()}> Search </button>
 
             <br /> <br />
             <p> Results </p>
+
+            {userNFTs.map((data) => 
+                <>
+                    <ImageBox>
+                        <p> Name: {data.name}</p>
+                        <img src={data.image_url} height={210} width={210}/>
+                        {/* {<p>Owner: {data.owner.user.username}</p>} */}
+                        {/* <p>TokenID: {data.description} </p> */}
+                        {/* <p>URL Link: {data.image_url} </p> */}
+                    </ImageBox>
+                </>
+            )}
+
         </Background>
         </>
     )
